ab52e5fab8a073a9ebf5ab8e83af7ab177d5402d
PuChen7 <puchen8295@gmail.com>
2018-04-03 17:04:45 -0400

    proof of work

--- blockchain/chain-src/block.test.js
+++ blockchain/chain-src/block.test.js
@@ -1,4 +1,5 @@
 const Block = require('./block');
+const {DIFFICULTY} = require('../config');  // get the DIFFICULTY in config.js
 
 describe('Block', () => {
     let data, lastBlock, block;
@@ -15,5 +16,10 @@ describe('Block', () => {
 
     it('sets the `lastHash` to match the last hash of the last block', () => {
         expect(block.lastHash).toEqual(lastBlock.hash);
+    });
+
+    it('generate a hash match the diff', () => {
+        expect(block.hash.substring(0, DIFFICULTY)).toEqual('0'.repeat(DIFFICULTY));
+        console.log(block.toString());
     });
 });
